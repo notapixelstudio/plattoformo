c6a9595a17bd22b9bcf25907717c0673e476fdc3
nitaku <matteo.abrate@gmail.com>
2019-10-04 12:33:43 +0200

    Better physics parametrs (sort of)

--- godot/Hero.gd
+++ godot/Hero.gd
@@ -30,14 +30,18 @@ func _physics_process(delta):
 	if action.in_state('ready'):
 		if env.in_state('floor') and Input.is_action_just_pressed("ui_accept"):
 			action.set_state('jump')
+		elif linear_velocity.y > 0:
+			action.set_state('fall')
 	elif action.in_state('jump'):
-		if Input.is_action_just_released("ui_accept") or linear_velocity.y >= 0:
+		if Input.is_action_just_released("ui_accept") or linear_velocity.y > 0:
 			action.set_state('fall')
 	elif env.in_state('floor'):
 		action.set_state('ready')
 		
-	apply_central_impulse(walk_speed*Vector2(int(Input.is_action_pressed('ui_right'))-int(Input.is_action_pressed('ui_left')),0))
-	$Sprite.flip_h = bool(Input.is_action_pressed('ui_left'))
+	var dir_x = int(Input.is_action_pressed('ui_right'))-int(Input.is_action_pressed('ui_left'))
+	apply_central_impulse(walk_speed*Vector2(dir_x,0))
+	if dir_x != 0:
+		$Sprite.flip_h = dir_x < 0
 	
 func _on_env_change(old_state, new_state):
 	print('Environment: ', new_state)
